0d256dc0484dbf3d4af95a69304b2cd18c22cd44
cheeese <143966422+TepliakovZakhariy@users.noreply.github.com>
2024-10-04 22:51:24 +0300

    added constructors

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,8 +1,12 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
 public class Flower {
     @Getter
@@ -13,14 +17,15 @@ public class Flower {
     @Getter
     private FlowerType flowerType;
 
-    public String getColor() {
-        return color.toString();
-    }
-
     public Flower(Flower flower) {
         this.sepalLength = flower.sepalLength;
         this.color = flower.color;
         this.price = flower.price;
         this.flowerType = flower.flowerType;
     }
+
+    public String getColor() {
+        return color.toString();
+    }
+
 }
